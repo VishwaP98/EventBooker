f178c4e0c27ac7e3fbafd5b4d76326e476c20355
Vishwa Patel <vishwa.patel@mail.utoronto.ca>
2019-03-11 17:45:11 -0400

    Summary : Added basic GraphQL functionality by developing schema and resolvers for queries and mutations. Author : Vishwa Patel

--- app.js
+++ app.js
@@ -1,12 +1,42 @@
-const express = require('express')
-const bodyParser = require('body-parser')
+const express = require('express');
+const bodyParser = require('body-parser');
+const graphQlHttp = require('express-graphql');
+const { buildSchema } = require('graphql');
 
 const app = express(); // uses express imported from express package
 
 app.use(bodyParser.json());
 
-app.get('/', (req, res, next) => {
-    res.send("Hello World, This is Vishwa");
-})
+app.use('/graphql', graphQlHttp({
+    schema: buildSchema(`
+
+        type RootQuery {
+            events: [String!]!
+        }
+
+        type RootMutation {
+            createEvent(name: String): String
+        }
+
+        schema {
+            query: RootQuery
+            mutation: RootMutation
+        }
+    
+    `),  // schema points to valid graphql schema
+    rootValue: {
+        events: () => {
+            return ['Event1', 'Event2', 'Event3']
+        },
+        createEvent: (args) => {
+            const eventName = args.name;
+            return eventName + " is good";
+        }
+
+    } // rootValue points to an object that contains all
+                  // the resolver functions
+    , graphiql: true
+}));
+
 
 app.listen(3000);
