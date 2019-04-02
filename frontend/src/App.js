747c8116dd7d34b2dfab6ae261998a1aed8b69fa
Vishwa Patel <vishwa.patel@mail.utoronto.ca>
2019-04-01 23:58:53 -0400

    Summary: Developed an initial component for User page and redirected initial page to User using BrowserRouter, Switch Author: Vishwa Patel

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react';
+
+import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
 import './App.css';
+import UserPage from './components/User';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-      </div>
+      <BrowserRouter>
+        <Switch>
+          <Redirect from="/" to="/user" exact/>
+          <Route path="/user" component={UserPage}/>
+          <Route path="/events" component={null}/>
+          <Route path="/bookings" component={null}/>
+        </Switch>
+       
+      
+      </BrowserRouter>
     );
   }
 }
