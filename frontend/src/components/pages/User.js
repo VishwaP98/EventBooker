f6b769d3c5fe4cfac29773c27bf4362ce9ecbd25
Vishwa Patel <vishwa.patel@mail.utoronto.ca>
2019-04-08 14:43:58 -0400

    Summary: Developed frontend for User Login/SignUp page along with styling. Author: Vishwa Patel

--- frontend/src/components/pages/User.js
+++ frontend/src/components/pages/User.js
@@ -1,9 +1,25 @@
 import React, { Component } from "react";
-
+import './User.css';
 class UserPage extends Component {
 
     render() {
-        return (<h1>User Page</h1>);
+        return (<form className="user-form">
+            <div className="form-control">
+                <label htmlFor="email">Email</label>
+                <input type="email" id="email"/>
+            </div>
+
+            <div className="form-control">
+                <label htmlFor="password">Password</label>
+                <input type="password" id="password"/>
+            </div>
+
+            <div className="form-actions">
+                <button type="submit">Submit</button>
+                <button type="button">SignUp</button>
+            </div>
+
+        </form>);
     }
 }
 
