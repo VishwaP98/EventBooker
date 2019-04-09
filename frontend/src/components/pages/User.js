6c6b7a822ecdc63b4f0a80930483c8f55552172e
Vishwa Patel <vishwa.patel@mail.utoronto.ca>
2019-04-09 09:45:07 -0400

    Summary: Developed logic for sending User Create requests to backend and added logic to handle incoming requests onto the backend. Author: Vishwa Patel

--- frontend/src/components/pages/User.js
+++ frontend/src/components/pages/User.js
@@ -2,16 +2,56 @@ import React, { Component } from "react";
 import './User.css';
 class UserPage extends Component {
 
+    constructor(props) {
+        super(props);
+        this.emailElement = React.createRef();
+        this.passwordElement = React.createRef();
+    }
+
+    submitHandler = (event) => {
+        event.preventDefault();
+        const email = this.emailElement.current.value;
+        const password = this.passwordElement.current.value;
+
+        if(email.trim().length === 0 || password.trim().length === 0) {
+            return; // we will not send empty email/password
+        }
+
+        console.log(email, password);
+
+        const requestBody = {
+
+            query: `
+                mutation {
+                    createUser(userInput: {email: "${email}", password: "${password}"}) {
+                        _id
+                        email
+                    }
+                }    
+            `
+
+        } 
+
+        // send a request to the backend
+        fetch("http://localhost:8000/graphql", {
+            method: "POST",
+            body: JSON.stringify(requestBody),
+            headers: {
+                "Content-Type": "application/json"
+            }
+        })
+    }
+
     render() {
-        return (<form className="user-form">
+        return (<form className="user-form" onSubmit={this.submitHandler}>
             <div className="form-control">
                 <label htmlFor="email">Email</label>
-                <input type="email" id="email"/>
+                <input type="email" id="email" ref={this.emailElement}/>
             </div>
 
             <div className="form-control">
                 <label htmlFor="password">Password</label>
-                <input type="password" id="password"/>
+                <input type="password" id="password" ref={this.passwordElement}/>
             </div>
 
             <div className="form-actions">
