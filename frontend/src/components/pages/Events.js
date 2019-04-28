b63ab1c43b06983411a2be8c6ee11302fc8984b0
Vishwa Patel <vishwa.patel@mail.utoronto.ca>
2019-04-27 23:25:34 -0400

    Summary: Added backdrop and ability to cancel and create event. Author: Vishwa Patel

--- frontend/src/components/pages/Events.js
+++ frontend/src/components/pages/Events.js
@@ -1,14 +1,43 @@
 import React, { Component } from "react";
 
 import './Events.css';
+import Modal from '../Modal/Modal';
+import Backdrop from '../Backdrop/Backdrop';
 
 class EventsPage extends Component {
+    state = {
+        creatingEvent: false
+    };
+
+    handleCreateEvent = () => {
+        this.setState({
+            creatingEvent: true
+        });
+    }
+
+    handleCancelEvent = () => {
+        this.setState({
+            creatingEvent: false
+        });
+    }
+
+    handleConfirmEvent = () => {
+        this.setState({
+            creatingEvent: false
+        });
+    }
 
     render() {
-        return (<div className="createEvent">
-            <p className="eventsHeader">Invite others to your event</p>
-            <button className="eventBtn">Create Event</button>
-        </div>);
+        return (
+            <React.Fragment>
+                {this.state.creatingEvent && <Backdrop/>}
+                {this.state.creatingEvent && <Modal title="Add Event" onCancel={this.handleCancelEvent} onConfirm={this.handleConfirmEvent}><p>Modal Content</p></Modal>}
+                <div className="createEvent">
+                    <p className="eventsHeader">Invite others to your event</p>
+                    <button className="eventBtn" onClick={this.handleCreateEvent}>Create Event</button>
+                </div>
+            
+            </React.Fragment>);
     }
 }
 
