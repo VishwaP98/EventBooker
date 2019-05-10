afff032f0d43d5477f0277173d79fee3c782c3b1
Vishwa Patel <vishwa.patel@mail.utoronto.ca>
2019-05-09 20:26:03 -0400

    Summary: Developed Bookings page to display bookings with option to cancel each and added a Spinner for loading. Author: Vishwa Patel

--- frontend/src/components/pages/Bookings.js
+++ frontend/src/components/pages/Bookings.js
@@ -1,9 +1,128 @@
 import React, { Component } from "react";
 
+import UserContext from '../Context/auth-context';
+import Spinner from '../Elements/Spinner/Spinner';
+import BookingList from '../Elements/BookingsList/BookingList';
+
 class BookingsPage extends Component {
 
+    state = {
+        isLoading: false,
+        bookings: []
+    };
+
+
+    static contextType = UserContext;
+
+    componentDidMount = () => {
+        this.fetchBookings();
+    }
+
+    cancelBookingHandler = (bookingId) => {
+        this.setState({isLoading: true});
+        const requestBody = {
+            query: `
+                mutation CancelBooking($id: ID!) {
+                    cancelBooking(bookingID: $id) {
+                        _id
+                        title
+                    }
+                }
+                `,
+            variables: {
+                id: bookingId
+            }
+        };
+
+        fetch('http://localhost:8000/graphql', {
+            method: 'POST',
+            body: JSON.stringify(requestBody),
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': "Bearer " + this.context.token
+            }
+        }).then(res => {
+            if(res.status !== 200 && res.status !== 201) {
+                throw new Error("Failed!");
+            }
+
+            return res.json();
+        }).then(resData => {
+            console.log(resData);
+            this.setState(prevState => {
+                // filter returns a new array, 
+                const latestBookings = prevState.bookings.filter(booking => {
+                    return booking._id !== bookingId; // true here means element will be kept, false means remove
+                });
+
+                return {bookings: latestBookings, isLoading: false};
+                
+            });
+        }).catch(err => {
+            console.log(err);
+            this.setState({isLoading: false});
+        });
+
+    }
+
+    fetchBookings = () => {
+
+        this.setState({isLoading: true});
+
+        const requestBody = {
+            query: `
+                query {
+                    bookings {
+                        _id
+                        createdAt
+                        event {
+                            _id
+                            title
+                            date
+                        }
+                    }
+                }
+                `
+        }; // define the request body
+
+        // need to send this request to the backend using fetch method
+
+        fetch('http://localhost:8000/graphql', {
+            method: 'POST',
+            body: JSON.stringify(requestBody),
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': "Bearer " + this.context.token
+            }
+        }).then(res => {
+            if(res.status !== 200 && res.status !== 201) {
+                throw new Error("Failed!");
+            }
+
+            return res.json();
+        }).then(resData => {
+            console.log(resData);
+            const bookings = resData.data.bookings;
+            this.setState({bookings: bookings, isLoading: false});
+        }).catch(err => {
+            console.log(err);
+            this.setState({isLoading: false});
+        });
+
+
+    }
+
     render() {
-        return (<h1>Bookings Page</h1>);
+        return (
+            
+            <React.Fragment>
+
+                {this.state.isLoading ? <Spinner/> : (
+                    <BookingList bookings={this.state.bookings} onCancel={this.cancelBookingHandler}/>
+                )}
+
+            </React.Fragment>
+        );
     }
 }
 
